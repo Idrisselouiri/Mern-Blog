b242d455970b299064f9343e95eb400d0b37829f
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-03-03 09:51:07 +0100

    connect to mongoDb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// connect mongoDb
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
+//listen server on port 3000
 app.listen(3000, () => {
   console.log("Sever is running on port 3000");
 });
